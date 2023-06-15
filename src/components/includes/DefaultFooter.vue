acd720e099ab8f93d217ff0cfe632c3645842324
Kendrick <kennyfagbenro44@gmail.com>
2023-06-15 11:06:50 +0100

    fixed build err

--- src/components/includes/DefaultFooter.vue
+++ src/components/includes/DefaultFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-primary pt-[4rem] md:pb-[4.5rem] md:pl-30 max-w-full">
+  <footer class="bg-primary pt-[4rem] md:pb-[4.5rem] md:pl-30">
     <div
       class="grid grid-cols-2 md:grid-cols-4 mx-auto w-full md:w-11/12 items-center border-b pb-10 border-secondary p-3"
     >
